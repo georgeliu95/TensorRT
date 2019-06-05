cb08f5114c5e8be03706a215debd4502b13056cb
TensorRT Service Account (Eric Work) <svc_tensorrt@nvidia.com>
2019-06-05 07:38:01 +0000

    [DIT/trunk] - TRT-8625 - Move kernelUtils.h to rtSafe. * Move ActivationType to NvInferRTSafe.h. * Switch kernelUtils.h and activation.h to use NvInferRTSafe.h. ====REVIEW==== #review @mvillmow @zhijial @jhalakp @jianweiz @jonasl @kkathpalia @msridhara @nwhitehead @pohanh @sanian @tali @yanchaol @yaoy ====MD5==== d41d8cd98f00b204e9800998ecf8427e
    Copied from Perforce
     Change: 26516102
    
    Mirrored from TensorRT at d7e0bdc918b2a2f77be76459299f434b7e2885d7
    
    Original Commit: d7e0bdc918b2a2f77be76459299f434b7e2885d7
    Remote URL: https://gitlab-master.nvidia.com/TensorRT/TensorRT.git
    Original Author: Ada Dong <adad@nvidia.com>
    Original Date: Wed Jun 5 00:14:33 2019 -0700
    Jenkins mirror URL: https://gpuwa.nvidia.com/hosting/jenkins/tensor-rt/job/OSS%20Mirroring/job/tensorrt-oss-mirror/1396/

--- include/NvInferRTSafe.h
+++ include/NvInferRTSafe.h
@@ -130,7 +130,26 @@ class ICudaEngine; //!< Forward declare safe::ICudaEngine for use in other inter
 class IExecutionContext; //!< Forward declare safe::IExecutionContext for use in other interfaces.
 }
 
-
+//!
+//! \enum ActivationType
+//!
+//! \brief Enumerates the types of activation to perform in an activation layer.
+//!
+enum class ActivationType : int
+{
+    kRELU = 0,             //!< Rectified linear activation.
+    kSIGMOID = 1,          //!< Sigmoid activation.
+    kTANH = 2,             //!< TanH activation.
+    kLEAKY_RELU = 3,       //!< LeakyRelu activation: x>=0 ? x : alpha * x.
+    kELU = 4,              //!< Elu activation: x>=0 ? x : alpha * (exp(x) - 1).
+    kSELU = 5,             //!< Selu activation: x>0 ? beta * x : beta * (alpha*exp(x) - alpha)
+    kSOFTSIGN = 6,         //!< Softsign activation: x / (1+|x|)
+    kSOFTPLUS = 7,         //!< Parametric softplus activation: alpha*log(exp(beta*x)+1)
+    kCLIP = 8,             //!< Clip activation: max(alpha, min(beta, x))
+    kHARD_SIGMOID = 9,     //!< Hard sigmoid activation: max(0, min(1, alpha*x+beta))
+    kSCALED_TANH = 10,     //!< Scaled tanh activation: alpha*tanh(beta*x)
+    kTHRESHOLDED_RELU = 11 //!< Thresholded ReLU activation: x>alpha : x : 0
+};
 
 template <typename T>
 constexpr inline int EnumMax(); //!< Maximum number of elements in an enumeration type.
