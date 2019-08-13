035fac414f3981221c910979e94c0128ec4b268d
TensorRT Service Account (Eric Work) <svc_tensorrt@nvidia.com>
2019-08-13 04:01:07 +0000

    [DIT/trunk] - None - 6.0.1.0- Increment TensorRT patch version to 1 and update build version to 0 Update TensorRT version to 6.0.1.0
    * Increment patch number in NvInferVersion.h
    
    #review-27010185: @ework
    
    Copied from Perforce
     Change: 27010250
    
    Mirrored from TensorRT at e67aa35496d15fef03b56f2dded7837a4420df64
    
    Original Commit: e67aa35496d15fef03b56f2dded7837a4420df64
    Remote URL: https://gitlab-master.nvidia.com/TensorRT/TensorRT.git
    Original Author: Sriya Rajagopalan <sriyar@nvidia.com>
    Original Date: Mon Aug 12 20:17:53 2019 -0700
    Jenkins mirror URL: https://gpuwa.nvidia.com/hosting-tensorrt/jenkins/tensor-rt/job/OSS%20Mirroring/job/tensorrt-oss-mirror/1336/

--- include/NvInferVersion.h
+++ include/NvInferVersion.h
@@ -52,11 +52,11 @@
 
 #define NV_TENSORRT_MAJOR 6 //!< TensorRT major version.
 #define NV_TENSORRT_MINOR 0 //!< TensorRT minor version.
-#define NV_TENSORRT_PATCH 0 //!< TensorRT patch version.
-#define NV_TENSORRT_BUILD 8 //!< TensorRT build number.
+#define NV_TENSORRT_PATCH 1 //!< TensorRT patch version.
+#define NV_TENSORRT_BUILD 0 //!< TensorRT build number.
 
 #define NV_TENSORRT_SONAME_MAJOR 6 //!< Shared object library major version number.
 #define NV_TENSORRT_SONAME_MINOR 0 //!< Shared object library minor version number.
-#define NV_TENSORRT_SONAME_PATCH 0 //!< Shared object library patch version number.
+#define NV_TENSORRT_SONAME_PATCH 1 //!< Shared object library patch version number.
 
 #endif // NV_INFER_VERSION_H
