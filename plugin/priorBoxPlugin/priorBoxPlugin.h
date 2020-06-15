78cf028725a009915cb9c65ef9cc936d626ed96c
Rajeev Rao <rajeevrao@nvidia.com>
2020-06-15 11:42:19 -0700

    Clang-format

--- plugin/priorBoxPlugin/priorBoxPlugin.h
+++ plugin/priorBoxPlugin/priorBoxPlugin.h
@@ -33,7 +33,8 @@ class PriorBox : public IPluginV2Ext
 public:
     PriorBox(PriorBoxParameters param);
 
-    PriorBox(PriorBoxParameters param, int numPriors, int H, int W, Weights minSize, Weights maxSize, Weights aspectRatios);
+    PriorBox(
+        PriorBoxParameters param, int numPriors, int H, int W, Weights minSize, Weights maxSize, Weights aspectRatios);
 
     PriorBox(const void* buffer, size_t length);
 
