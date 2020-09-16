f19dcb9e9b83db3786831a558c521c42d18040be
Vincent Huang <vincenth@nvidia.com>
2020-09-15 19:18:53 -0700

    reword the error message

--- demo/BERT/builder.py
+++ demo/BERT/builder.py
@@ -670,9 +670,9 @@ def main():
 
     cc = pycuda.autoinit.device.compute_capability()
     if cc[0] * 10 + cc[1] < 75 and args.force_int8_multihead:
-        raise RuntimeError("The option --force-int8-multihead only support Turing+ GPU.")
+        raise RuntimeError("--force-int8-multihead option is only supported on Turing+ GPU.")
     if cc[0] * 10 + cc[1] < 72 and args.force_int8_skipln:
-        raise RuntimeError("The option --force-int8-skipln only support Xavier+ GPU.")
+        raise RuntimeError("--force-int8-skipln option is only supported on Xavier+ GPU.")
 
     bert_config_path = os.path.join(args.config_dir, "bert_config.json")
     TRT_LOGGER.log(TRT_LOGGER.INFO, "Using configuration file: {:}".format(bert_config_path))
