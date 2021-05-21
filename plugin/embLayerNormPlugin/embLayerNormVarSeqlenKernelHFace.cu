3abdbbe55986499955933cc507b2326dd0ae4a4d
Rajeev Rao <rajeevrao@nvidia.com>
2021-05-20 22:59:58 -0700

    Update embLayerNormPlugin to rel-8.0 API

--- plugin/embLayerNormPlugin/embLayerNormVarSeqlenKernelHFace.cu
+++ plugin/embLayerNormPlugin/embLayerNormVarSeqlenKernelHFace.cu
@@ -22,7 +22,7 @@
 #include "NvInfer.h"
 #include "bertCommon.h"
 #include "common.cuh"
-#include "embLayerNormPlugin.h"
+#include "plugin.h"
 #include "serialize.hpp"
 
 using namespace nvinfer1;
@@ -31,10 +31,10 @@ namespace bert
 {
 
 __global__ void cuSeqlensToPackedMaskKernel(
-    const uint32_t warps_m, const uint32_t warps_n, const uint32_t S, const int* cuSeqlens, uint32_t* inputMaskX)
+    const uint32_t warps_m, const uint32_t warps_n, const uint32_t S, const int32_t* cuSeqlens, uint32_t* inputMaskX)
 {
 
-    extern __shared__ int shm_mask[]; // S mask elements of this batch
+    extern __shared__ int32_t shm_mask[]; // S mask elements of this batch
 
     const size_t xmmas_n = (S + 16 * warps_n - 1) / (16 * warps_n);
     const uint32_t threads_per_cta = blockDim.x;
@@ -64,7 +64,7 @@ __global__ void cuSeqlensToPackedMaskKernel(
 
     for (size_t ni = 0; ni < xmmas_n; ++ni)
     {
-        const int offset = ni * 16 * warps_n + col;
+        const int32_t offset = ni * 16 * warps_n + col;
         mask |= (shm_mask[offset + 0] == 1 ? 1u : 0u) << (8 * ni + 0);
         mask |= (shm_mask[offset + 1] == 1 ? 1u : 0u) << (8 * ni + 1);
         mask |= (shm_mask[offset + 0] == 1 ? 1u : 0u) << (8 * ni + 2);
@@ -79,45 +79,46 @@ __global__ void cuSeqlensToPackedMaskKernel(
 }
 
 void cuSeqlensToPackedMask(const uint32_t S, const uint32_t B, const uint32_t warps_m, const uint32_t warps_n,
-    const uint32_t warps_k, const int* cuSeqlens, uint32_t* inputMaskX, cudaStream_t stream)
+    const uint32_t warps_k, const int32_t* cuSeqlens, uint32_t* inputMaskX, cudaStream_t stream)
 {
     const size_t xmmas_m = (S + 16 * warps_m - 1) / (16 * warps_m);
 
     const size_t threads_per_cta = warps_m * warps_n * warps_k * 32;
     dim3 grid(xmmas_m, B);
-    cuSeqlensToPackedMaskKernel<<<grid, threads_per_cta, S * sizeof(int), stream>>>(
+    cuSeqlensToPackedMaskKernel<<<grid, threads_per_cta, S * sizeof(int32_t), stream>>>(
         warps_m, warps_n, S, cuSeqlens, inputMaskX);
     CHECK(cudaPeekAtLastError());
 }
 
 template <typename T, unsigned TPB, unsigned VPT>
-__global__ void embLayerNormKernelVarSeqlen(int ld, const uint32_t* cuSeqlens, const int* inputIds,
-    const int* segmentIds, const T* beta, const T* gamma, const T* tokEmb, const T* posEmb, const T* segEmb, T* output)
+__global__ void embLayerNormKernelVarSeqlenHFace(int32_t ld, const uint32_t* cuSeqlens, const int32_t* inputIds,
+    const int32_t* segmentIds, const T* beta, const T* gamma, const T* tokEmb, const T* posEmb, const T* segEmb,
+    T* output)
 {
 
     using BlockReduce = cub::BlockReduce<kvp<T>, TPB>;
     __shared__ typename BlockReduce::TempStorage temp_storage;
 
-    const int b = blockIdx.x;
-    const int s = blockIdx.y;
+    const int32_t b = blockIdx.x;
+    const int32_t s = blockIdx.y;
 
-    const int sum_s = cuSeqlens[b];
-    const int s_b = cuSeqlens[b + 1] - sum_s;
+    const int32_t sum_s = cuSeqlens[b];
+    const int32_t s_b = cuSeqlens[b + 1] - sum_s;
 
     // either the whole CTA has work or not
     if (s >= s_b)
         return;
 
-    const int inOffset = (sum_s + s);
-    const int outOffset = (sum_s + s) * ld;
+    const int32_t inOffset = (sum_s + s);
+    const int32_t outOffset = (sum_s + s) * ld;
 
     // 1. lookup word and token of the block
     // blockIdx.x = position in the sequence
     // blockIdx.y = batch
     // gridDim.x = S
     // gridDim.y = B
-    __shared__ int inputId;
-    __shared__ int segmentId;
+    __shared__ int32_t inputId;
+    __shared__ int32_t segmentId;
 
     if (threadIdx.x == 0)
     {
@@ -128,14 +129,14 @@ __global__ void embLayerNormKernelVarSeqlen(int ld, const uint32_t* cuSeqlens, c
 
     // 2. load pos/tok/word embeddings and add them toghether
     // offset into embeddings is given by wordId * hidden_size
-    const int poffset = s * ld;
-    const int ioffset = inputId * ld;
-    const int soffset = segmentId * ld;
+    const int32_t poffset = s * ld;
+    const int32_t ioffset = inputId * ld;
+    const int32_t soffset = segmentId * ld;
 
     // 16B per thread: 8 elements. there should be ld / VPT threads per CTA
     // 1024: 128 threads
     // 768: 96 threads
-    const int toffset = threadIdx.x * VPT;
+    const int32_t toffset = threadIdx.x * VPT;
     // 4 * 1024 * 4 * 2 Bytes = 16KB per block
     T i_local[VPT];
     T s_local[VPT];
@@ -150,7 +151,7 @@ __global__ void embLayerNormKernelVarSeqlen(int ld, const uint32_t* cuSeqlens, c
 
     const T rld = T(1) / T(ld);
 #pragma unroll
-    for (int it = 0; it < VPT; it++)
+    for (int32_t it = 0; it < VPT; it++)
     {
         i_local[it] += s_local[it] + p_local[it];
         const T tmp = rld * i_local[it];
@@ -175,7 +176,7 @@ __global__ void embLayerNormKernelVarSeqlen(int ld, const uint32_t* cuSeqlens, c
     __syncthreads();
     ///*
 #pragma unroll
-    for (int it = 0; it < VPT; it++)
+    for (int32_t it = 0; it < VPT; it++)
     {
         i_local[it] = s_local[it] * (i_local[it] - mu) * rsigma + p_local[it];
     }
@@ -185,26 +186,27 @@ __global__ void embLayerNormKernelVarSeqlen(int ld, const uint32_t* cuSeqlens, c
 }
 
 template <typename T>
-int embSkipLayerNormVarSeqlen(cudaStream_t stream, int ld, int B, int S, const uint32_t* cuSeqlens, const int* inputIds,
-    const int* token_ids, const T* beta, const T* gamma, const T* wordEmb, const T* posEmb, const T* tokEmb, T* output)
+int32_t embSkipLayerNormVarSeqlenHFace(cudaStream_t stream, int32_t ld, int32_t B, int32_t S, const uint32_t* cuSeqlens,
+    const int32_t* inputIds, const int32_t* token_ids, const T* beta, const T* gamma, const T* wordEmb, const T* posEmb,
+    const T* tokEmb, T* output)
 {
 
     const dim3 grid(B, S, 1);
 
     if (ld == 1024)
     {
-        constexpr int VPT = 16 / sizeof(T);
-        constexpr int TPB = 1024 / VPT;
+        constexpr int32_t VPT = 16 / sizeof(T);
+        constexpr int32_t TPB = 1024 / VPT;
         const dim3 block(TPB, 1, 1);
-        embLayerNormKernelVarSeqlen<T, TPB, VPT><<<grid, block, 0, stream>>>(
+        embLayerNormKernelVarSeqlenHFace<T, TPB, VPT><<<grid, block, 0, stream>>>(
             ld, cuSeqlens, inputIds, token_ids, beta, gamma, wordEmb, posEmb, tokEmb, output);
     }
     else if (ld == 768)
     {
-        constexpr int VPT = 16 / sizeof(T);
-        constexpr int TPB = 768 / VPT;
+        constexpr int32_t VPT = 16 / sizeof(T);
+        constexpr int32_t TPB = 768 / VPT;
         const dim3 block(TPB, 1, 1);
-        embLayerNormKernelVarSeqlen<T, TPB, VPT><<<grid, block, 0, stream>>>(
+        embLayerNormKernelVarSeqlenHFace<T, TPB, VPT><<<grid, block, 0, stream>>>(
             ld, cuSeqlens, inputIds, token_ids, beta, gamma, wordEmb, posEmb, tokEmb, output);
     }
     else
@@ -217,17 +219,18 @@ int embSkipLayerNormVarSeqlen(cudaStream_t stream, int ld, int B, int S, const u
     return 0;
 }
 
-template int embSkipLayerNormVarSeqlen<float>(cudaStream_t, int, int, int, const uint32_t*, const int*, const int*,
-    const float*, const float*, const float*, const float*, const float*, float*);
+template int32_t embSkipLayerNormVarSeqlenHFace<float>(cudaStream_t, int32_t, int32_t, int32_t, const uint32_t*,
+    const int32_t*, const int32_t*, const float*, const float*, const float*, const float*, const float*, float*);
 
-template int embSkipLayerNormVarSeqlen<half>(cudaStream_t, int, int, int, const uint32_t*, const int*, const int*,
-    const half*, const half*, const half*, const half*, const half*, half*);
+template int32_t embSkipLayerNormVarSeqlenHFace<half>(cudaStream_t, int32_t, int32_t, int32_t, const uint32_t*,
+    const int32_t*, const int32_t*, const half*, const half*, const half*, const half*, const half*, half*);
 
 /// REDO BASED ON OLD KERNEL TO REPRODUCE EXACT RESULTS
 
 template <typename T, unsigned TPB>
-__global__ void embLayerNormKernel2(int ld, const int* inputIds, const int* tokenIds, const int* cuSeqlens,
-    const float* beta, const float* gamma, const T* wordEmb, const T* posEmb, const T* tokEmb, T* output)
+__global__ void embLayerNormKernelHFace(int32_t ld, const int32_t* inputIds, const int32_t* tokenIds,
+    const int32_t* cuSeqlens, const float* beta, const float* gamma, const T* wordEmb, const T* posEmb, const T* tokEmb,
+    T* output)
 {
     // this code currently assumes the input shape is SxB, row-major => seqPos = s * B + b
     // instead we want BxS, row-major => seqPos = b * S + s
@@ -238,22 +241,22 @@ __global__ void embLayerNormKernel2(int ld, const int* inputIds, const int* toke
     // blockIdx.y = batch
     // gridDim.x = S
     // gridDim.y = B
-    const int s = blockIdx.x;
-    const int b = blockIdx.y;
+    const int32_t s = blockIdx.x;
+    const int32_t b = blockIdx.y;
 
-    const int sumS = cuSeqlens[b];
-    const int s_b = cuSeqlens[b + 1] - sumS;
+    const int32_t sumS = cuSeqlens[b];
+    const int32_t s_b = cuSeqlens[b + 1] - sumS;
     if (s >= s_b)
         return; // This CTA has nothing to do
-    __shared__ int wordId;
-    __shared__ int tokenId;
+    __shared__ int32_t wordId;
+    __shared__ int32_t tokenId;
 
     const T rld = T(1.f) / T(ld);
     // seqPos = b + s * B
-    // const int seqPos = blockIdx.y + blockIdx.x * gridDim.y;
+    // const int32_t seqPos = blockIdx.y + blockIdx.x * gridDim.y;
 
-    // const int seqPos = s * B + s;
-    const int seqPos = sumS + s;
+    // const int32_t seqPos = s * B + s;
+    const int32_t seqPos = sumS + s;
     if (threadIdx.x == 0)
     {
         wordId = inputIds[seqPos];
@@ -263,15 +266,15 @@ __global__ void embLayerNormKernel2(int ld, const int* inputIds, const int* toke
 
     // 2. load pos/tok/word embeddings and add them toghether
     // offset into embeddings is given by wordId * hidden_size
-    const int poffset = blockIdx.x * ld;
-    const int woffset = wordId * ld;
-    const int toffset = tokenId * ld;
+    const int32_t poffset = blockIdx.x * ld;
+    const int32_t woffset = wordId * ld;
+    const int32_t toffset = tokenId * ld;
     // the output offset is given by b * (S*hidden_size) + s * hidden_size
-    const int outOffset = seqPos * ld;
+    const int32_t outOffset = seqPos * ld;
 
     kvp<T> threadData(0, 0);
 
-    for (int it = threadIdx.x; it < ld; it += TPB)
+    for (int32_t it = threadIdx.x; it < ld; it += TPB)
     {
         const T w(wordEmb[woffset + it]);
         const T t(tokEmb[toffset + it]);
@@ -288,26 +291,24 @@ __global__ void embLayerNormKernel2(int ld, const int* inputIds, const int* toke
 }
 
 template <typename T>
-int embSkipLayerNorm2(cudaStream_t stream, int ld, int B, int S, const int* inputIds, const int* tokenIds,
-    const int* cuSeqlens, const float* beta, const float* gamma, const T* wordEmb, const T* posEmb, const T* tokEmb,
-    T* output)
+int32_t embSkipLayerNormHFace(cudaStream_t stream, int32_t ld, int32_t B, int32_t S, const int32_t* inputIds,
+    const int32_t* tokenIds, const int32_t* cuSeqlens, const float* beta, const float* gamma, const T* wordEmb,
+    const T* posEmb, const T* tokEmb, T* output)
 {
 
-    constexpr int tpb = 256;
+    constexpr int32_t tpb = 256;
     const dim3 grid(S, B, 1);
     const dim3 block(tpb, 1, 1);
 
-    embLayerNormKernel2<T, tpb>
+    embLayerNormKernelHFace<T, tpb>
         <<<grid, block, 0, stream>>>(ld, inputIds, tokenIds, cuSeqlens, beta, gamma, wordEmb, posEmb, tokEmb, output);
-    CHECK(cudaPeekAtLastError());
-
-    return 0;
+    return cudaPeekAtLastError();
 }
 
-template int embSkipLayerNorm2<float>(cudaStream_t, int, int, int, const int*, const int*, const int*, const float*,
-    const float*, const float*, const float*, const float*, float*);
+template int32_t embSkipLayerNormHFace<float>(cudaStream_t, int32_t, int32_t, int32_t, const int32_t*, const int32_t*,
+    const int32_t*, const float*, const float*, const float*, const float*, const float*, float*);
 
-template int embSkipLayerNorm2<half>(cudaStream_t, int, int, int, const int*, const int*, const int*, const float*,
-    const float*, const half*, const half*, const half*, half*);
+template int32_t embSkipLayerNormHFace<half>(cudaStream_t, int32_t, int32_t, int32_t, const int32_t*, const int32_t*,
+    const int32_t*, const float*, const float*, const half*, const half*, const half*, half*);
 
 } // namespace bert
