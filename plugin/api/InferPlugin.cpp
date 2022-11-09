5bbac650c308dec998d79c60e6b1f5717081c828
Samurdhi Karunaratne <skarunaratne@nvidia.com>
2022-11-08 20:33:32 -0800

    Add demo diffusion plugins

--- plugin/api/InferPlugin.cpp
+++ plugin/api/InferPlugin.cpp
@@ -41,7 +41,9 @@ using namespace nvinfer1::plugin;
 #include "flattenConcat.h"
 #include "generateDetectionPlugin.h"
 #include "gridAnchorPlugin.h"
+#include "groupNormPlugin.h"
 #include "instanceNormalizationPlugin.h"
+#include "layerNormPlugin.h"
 #include "lReluPlugin.h"
 #include "multilevelCropAndResizePlugin.h"
 #include "multilevelProposeROIPlugin.h"
@@ -61,6 +63,7 @@ using namespace nvinfer1::plugin;
 #include "scatterPlugin.h"
 #include "specialSlicePlugin.h"
 #include "split.h"
+#include "splitGeLUPlugin.h"
 #include "voxelGenerator.h"
 
 using nvinfer1::plugin::RPROIParams;
@@ -188,7 +191,9 @@ extern "C"
         initializePlugin<nvinfer1::plugin::GenerateDetectionPluginCreator>(logger, libNamespace);
         initializePlugin<nvinfer1::plugin::GridAnchorPluginCreator>(logger, libNamespace);
         initializePlugin<nvinfer1::plugin::GridAnchorRectPluginCreator>(logger, libNamespace);
+        initializePlugin<nvinfer1::plugin::GroupNormPluginCreator>(logger, libNamespace);
         initializePlugin<nvinfer1::plugin::InstanceNormalizationPluginCreator>(logger, libNamespace);
+        initializePlugin<nvinfer1::plugin::LayerNormPluginCreator>(logger, libNamespace);
         initializePlugin<nvinfer1::plugin::LReluPluginCreator>(logger, libNamespace);
         initializePlugin<nvinfer1::plugin::MultilevelCropAndResizePluginCreator>(logger, libNamespace);
         initializePlugin<nvinfer1::plugin::MultilevelProposeROIPluginCreator>(logger, libNamespace);
@@ -209,6 +214,7 @@ extern "C"
         initializePlugin<nvinfer1::plugin::ROIAlignPluginCreator>(logger, libNamespace);
         initializePlugin<nvinfer1::plugin::ScatterNDPluginCreator>(logger, libNamespace);
         initializePlugin<nvinfer1::plugin::SpecialSlicePluginCreator>(logger, libNamespace);
+        initializePlugin<nvinfer1::plugin::SplitGeLUPluginCreator>(logger, libNamespace);
         initializePlugin<nvinfer1::plugin::SplitPluginCreator>(logger, libNamespace);
         initializePlugin<nvinfer1::plugin::VoxelGeneratorPluginCreator>(logger, libNamespace);
         return true;
