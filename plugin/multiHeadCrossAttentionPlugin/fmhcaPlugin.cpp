d2bc0b871927210f1e58886964cb65e8d076b12b
Vincent Huang <vincenth@nvidia.com>
2022-11-10 06:27:11 +0000

    do not use ostringstream in enqueue for perf

--- plugin/multiHeadCrossAttentionPlugin/fmhcaPlugin.cpp
+++ plugin/multiHeadCrossAttentionPlugin/fmhcaPlugin.cpp
@@ -1,13 +1,6 @@
 #include "fmhcaPlugin.h"
 #include "fmhca.h"
 
-/*
-   input[ 0]:  [float16],  [b, s_q,  h, d] // Q
-   input[ 1]:  [float16],  [b, s_kv, h, 2, d] // K and V
-
-   output[0]:  [float16],  (b, s_q, h, d)
-   */
-
 using namespace nvinfer1;
 using namespace plugin;
 
@@ -35,27 +28,15 @@ int32_t fmhcaPlugin::enqueue(const PluginTensorDesc* inputDesc, const PluginTens
         // Check for seq len to support dynamic input shape
         if (sizePerHead <= 64)
         {
-            std::ostringstream oss;
-            oss << "Not support q buffer sequence length not multiple of 64 when head size < 64 for plugin "
-                << PLUGIN_NAME << " (q = " << seqLenQ << ", headSize = " << sizePerHead << ")" << std::endl;
-
-            PLUGIN_VALIDATE(seqLenQ % 64 == 0, oss.str().c_str());
+            PLUGIN_VALIDATE(seqLenQ % 64 == 0, "Not support q buffer sequence length not multiple of 64 when head size < 64 for plugin fMHCA");
         }
         else if (sizePerHead <= 128)
         {
-            std::ostringstream oss;
-            oss << "Not support q buffer sequence length not multiple of 32 when head size < 128 for plugin "
-                << PLUGIN_NAME << " (q = " << seqLenQ << ", headSize = " << sizePerHead << ")" << std::endl;
-
-            PLUGIN_VALIDATE(seqLenQ % 32 == 0, oss.str().c_str());
+            PLUGIN_VALIDATE(seqLenQ % 32 == 0, "Not support q buffer sequence length not multiple of 32 when head size between 64 and 128 for plugin fMHCA");
         }
         else
         {
-            std::ostringstream oss;
-            oss << "Not support q buffer sequence length not multiple of 16 for plugin " << PLUGIN_NAME
-                << " (q = " << seqLenQ << ", headSize = " << sizePerHead << ")" << std::endl;
-
-            PLUGIN_VALIDATE(seqLenQ % 16 == 0, oss.str().c_str());
+            PLUGIN_VALIDATE(seqLenQ % 16 == 0, "Not support q buffer sequence length not multiple of 16 when head size > 128 for plugin fMHCA");
         }
 
         if (batchSize != m_.mOptBatchSize || m_.mOptSeqLenQ != seqLenQ || m_.mOptSeqLenKV != seqLenKV)
